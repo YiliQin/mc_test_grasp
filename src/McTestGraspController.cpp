e1be68a53bd48213643816c28b56ffe0a4603779
Yili Qin <yili.jrl@gmail.com>
2021-10-27 14:37:35 +0900

    add tasks

--- src/McTestGraspController.cpp
+++ src/McTestGraspController.cpp
@@ -1,10 +1,19 @@
 #include "McTestGraspController.h"
+#include <mc_tasks/MetaTaskLoader.h>
 
 McTestGraspController::McTestGraspController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
 : mc_control::fsm::Controller(rm, dt, config)
 {
 
   mc_rtc::log::success("McTestGraspController init done ");
+
+  // add tasks for solver
+  comTask = mc_tasks::MetaTaskLoader::load<mc_tasks::CoMTask>(solver(), config("CoM_task"));
+
+  leftHandTask.reset(new mc_tasks::SurfaceTransformTask("LeftGripper", robots(), robots().robotIndex(), 2.0, 500));
+
+  rightHandTask.reset(new mc_tasks::SurfaceTransformTask("RightGripper", robots(), robots().robotIndex(), 2.0, 500));
+
 }
 
 bool McTestGraspController::run()
--- src/McTestGraspController.h
+++ src/McTestGraspController.h
@@ -2,7 +2,8 @@
 
 #include <mc_control/mc_controller.h>
 #include <mc_control/fsm/Controller.h>
-
+#include <mc_tasks/CoMTask.h>
+#include <mc_tasks/SurfaceTransformTask.h>
 #include "api.h"
 
 struct McTestGraspController_DLLAPI McTestGraspController : public mc_control::fsm::Controller
@@ -12,6 +13,12 @@ struct McTestGraspController_DLLAPI McTestGraspController : public mc_control::f
     bool run() override;
 
     void reset(const mc_control::ControllerResetData & reset_data) override;
+    
+    // Tasks
+    std::shared_ptr<mc_tasks::CoMTask> comTask;
+    std::shared_ptr<mc_tasks::SurfaceTransformTask> leftHandTask;
+		std::shared_ptr<mc_tasks::SurfaceTransformTask> rightHandTask;
+
 private:
     mc_rtc::Configuration config_;
 };
